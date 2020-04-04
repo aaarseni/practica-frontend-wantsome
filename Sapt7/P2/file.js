6846d13352861a4840cef483afc1f918597c645b
aaarseni <alexandru.arseni@yahoo.com>
2020-04-04 16:23:37 +0300

    Adding homework week7 p2

--- Sapt7/P2/file.js
+++ Sapt7/P2/file.js
@@ -1,5 +1,6 @@
-//ex1
-
+/* 1. Scrieti o functie care sa protejeze emailul unui user
+de ex: myFunction("ovidiu.grigoras@test.com") sa printeze "ovidiu...@test.com" sau "ovid...@test.com"
+ */
 var emailAddress = function (eMail) {
     var sign = eMail.split('@');
     var secondSign = sign[1].split('.');
@@ -10,21 +11,19 @@ console.log(emailAddress('arseni.alexandru@yahoo.com'));
 
 
 
-//ex2
-var myFunction = function (string) {
+/* 2. Scrieti o functie care sa faca uppercase la fiecare litera de la fiecare inceput de string
+ de ex: myFunction("i am superman") sa printeze "I Am Superman */
+ var myFunction = function (string) {
     var res = string.split(' ');
-    console.log(res);
-    for (i = 0; i < res.lenght; i++) {
+    for (i = 0; i < res.length; i++) {
         res[i] = res[i].charAt(0).toUpperCase() + res[i].slice(1);
     }
     console.log(res.join(' '));
+};
+myFunction("i am superman and batman, the cat is meow");
 
-    console.log(res[0] + ' ' + res[1] + ' ' + res[2]);
-}
-myFunction("i am superman and batman");
-
-
-//ex3
+/* //3. Scrieti o functie care sa schimbe literele unui string daca sunt uppercase cu lowercase si invers
+ex: myFunction("xxXyYzZZ") sa printeze "XXxYyZzz" */
 function firstString(string) {
     return string.replace(/([a-z]+)|([A-Z]+)/g, function (match, chr) {
         return chr ? match.toUpperCase() : match.toLowerCase();
@@ -49,7 +48,8 @@ function switchLetter(string) {
 console.log(switchLetter('sjkJKHDdnKsd'));
 
 
-//ex4
+/* //4. Scrieti o functie care sa concateneze un string de cate ori ii zicem
+de ex: myFunction("Wantsome", 2) sa printeze "WantsomeWantsome */
 function moreStrings(string, number) {
     var result = "";
     for (i = 0; i < number; i++) {
@@ -57,7 +57,7 @@ function moreStrings(string, number) {
     }
     console.log(result);
 }
-moreStrings('wantsome', 2);
+moreStrings('wantsome ', 4);
 
 
 //ex5
@@ -73,16 +73,31 @@ function palindrom(item) {
 };
 console.log(palindrom('cal'));
 
-//6. Implementati o functie care accepta ca argument un array compus din mai multe array-uri de valori numerice si
-// returneaza un array care contine ca si elemente cele mai mari numere din fiecare array
-var item = [1, 3, 6, 10, 14,];
-var item1 = [34, 23, 55, 1, 7];
+/* //6. Implementati o functie care accepta ca argument un array compus din mai multe array-uri de valori numerice si
+ returneaza un array care contine ca si elemente cele mai mari numere din fiecare array */
+//Am doua array pe care le voi sorta descrescator
+//Valoarea cea mai mare care imi iese de la fiecare array le voi introduce intr-o functie
+// unde parametrul functiei va primi ca valoare concatenamentul valorilor cu index 0 de la cele doua array uri.
 
-function arraySort(array) {
+const returnTheBiggestValue = (item, item1) => {
     item.sort(function (a, b) {
         return b - a;
     });
-}
+    if (item[1] < item[0]) {
+        console.log(item[0]);
+    };
+
+    item1.sort(function (a, b) {
+        return b - a;
+    });
+    if (item1[1] < item1[0]) {
+        return item1[0];
+    };
+};
+console.log(returnTheBiggestValue([149, 3, 9, 10, 143,], [344, 23, 55, 1, 7]))
+//let item = [14, 3, 9, 10, 143,];
+//let item1 = [34, 23, 55, 1, 7];
+
 //////////////////////////////////////
 
 
@@ -94,3 +109,105 @@ var bigString = function (string) {
 console.log(bigString('Mai am multe de invatat'));
 
 //8. Implementati o functie care calculeaza factorialul unui numar
+function factorial(nr) {
+    for (i = nr - 1; i >= 1; i--) {
+        nr *= i
+    }
+    return nr;
+};
+console.log(factorial(5));
+
+//9. Implementati o functie care accepta ca argumente doua string-uri si verifica daca primul se termina cu cel din urma
+const checkStr = (str1, str2) => {
+    if (str1.endsWith(str2)) {
+        return true;
+    }
+    return false;
+}
+console.log(checkStr('caruta', 'ta'));
+
+
+/* 10. Implementati o functie care accepta doua argumente: un array si o functie de adevar. Functia returneaza primul
+element din array care trece testul specificat */
+const functionTest = (arrayArg, functionArg) => {
+    for (i = 0; i < arrayArg.length; i++) {
+        functionArg(arrayArg[i]);
+    }
+};
+
+let arrayListTest = [1, 2, 3, 4, 5, 6, 7];
+
+const testArray = (aNumber) => {
+    if (aNumber === 3) {
+        console.log(aNumber);
+    }
+}
+functionTest(arrayListTest, testArray);
+
+/* 11. Implementati o functie care accepta ca argumente doua string-uri si verifica daca primul string contine toate literele
+celui de-al doilea string */
+const checkString = (string1, strin2) => {
+    if (string1 === strin2) {
+        return true;
+    }
+    return false;
+}
+console.log(checkString('ala', 'ala'));
+
+/* 12. Implementati o functie care accepta ca argumente doi parametri: un array si o valoare. Functia afiseaza fiecare
+element al array-ului pana cand intalneste elementul cu valoarea specificata */
+const listWord = (word, number) => {
+    let someWords = [];
+    for (i = 0; i < word.length; i++) {
+        someWords = word[i];
+        console.log(someWords)
+        if (someWords === number) {
+            break;
+        };
+    }//return someWords;
+};
+console.log(listWord([1, 2, 3, 4, 5, 6], 4));
+//----
+const listWord = (word, number) => {
+    let someWords = [];
+    for (someWords of word) {
+        console.log(someWords)
+        someWords === word;
+        if (someWords === number)
+            break;
+    };
+    //return someWords;
+};
+console.log(listWord([1, 2, 3, 4, 5,], 3));
+
+
+/* 13. Scrieti o functie care elimina toate valorile false dintr-un array: false, null, 0, "", undefined, NaN */
+let oldArray = [false, null, 0, "", undefined, NaN, 1];
+const deleteValueFromArray = (array) => {
+    newArray = []
+    for (i = 0; i < array.length; i++) {
+        if (Boolean(array[i]) === true) {
+            newArray.push(array[i]);
+        }
+    };
+    console.log(newArray);
+};
+deleteValueFromArray(oldArray);
+//---------
+let oldArray = [false, null, 0, "", undefined, NaN, 1];
+const deleteValueFromArray = (array) => {
+    newArray = []
+    for (i = 0; i < array.length; i++) {
+        newArray.push(!!array[i]);
+    };
+    console.log(newArray);
+};
+deleteValueFromArray(oldArray);
+
+/*14. Scrieti o functie care repeta un string de n ori specificate */
+const repeatString = (string, aBigNumber) => {
+    do {
+        return string.repeat(aBigNumber);
+    } while (aBigNumber)
+};
+console.log(repeatString('Azi nu mai jucam fotbal / ', 4));
