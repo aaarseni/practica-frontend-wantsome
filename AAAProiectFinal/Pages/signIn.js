34768e7c4e634dd94300db1dd70a002e89373b2b
aaarseni <alexandru.arseni@yahoo.com>
2020-06-11 20:57:17 +0300

    deleting files

--- AAAProiectFinal/Pages/signIn.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    $('#pageForm').on('click', function(e){
-        e.preventDefault();
-        var link = $(this).attr('href');
-        $('.page').load(link);
-    });
-
-});
